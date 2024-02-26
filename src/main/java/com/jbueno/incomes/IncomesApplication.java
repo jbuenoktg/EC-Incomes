8449052125478c1d9d42a590c40c1fc020649f73
Juan Jesus Bueno Reyes <jesus.bueno.reyes@gmail.com>
2024-02-25 21:14:55 -0600

    Creating incomes module

---
+++ src/main/java/com/jbueno/incomes/IncomesApplication.java
@@ -0,0 +1,13 @@
+package com.jbueno.incomes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jbueno/incomes/IncomesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jbueno.incomes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
